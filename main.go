751a84c3f71fc884ff90fd4c5106d85786263ddb
bthedub <bthedub@gmail.com>
2024-05-07 13:34:23 -0600

    final

--- main.go
+++ main.go
@@ -11,21 +11,30 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 )
 
-var buf *bytes.Buffer
+type Location struct {
+	URL string `json:"location"`
+}
+
+type Payload struct {
+	Data string `json:"data"`
+}
 
 func main() {
 	data, err := os.ReadFile("./input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	lines := strings.Split(string(data), "\n")
 	for _, line := range lines {
 		data, err := getData(line)
 		if err != nil {
-			log.Printf("unable to get status: %v", err)
+			fmt.Printf("unable to get status: %v", err)
+			continue
 		}
 		if data.(string) == "foo" {
 			fmt.Printf("data found: %s", data)
@@ -35,31 +44,88 @@ func main() {
 }
 
 func getData(line string) (any, error) {
-	var location struct{ URL string }
+	var location Location
+	var payload Payload
+	var buf *bytes.Buffer = &bytes.Buffer{}
+
 	err := json.Unmarshal([]byte(line), &location)
 	if err != nil {
-		return nil, err
+		return "", fmt.Errorf("failed to unmarshal line: %w", err)
 	}
+
+	if !strings.HasPrefix(location.URL, "http://") && !strings.HasPrefix(location.URL, "https://") {
+		location.URL = "http://" + location.URL
+	}
+
+	re := regexp.MustCompile(`^https?://`)
+	if !re.MatchString(location.URL) {
+		noColonSlash := regexp.MustCompile(`^https?[^://]`)
+		if noColonSlash.MatchString(location.URL) {
+			switch {
+			case strings.HasPrefix(location.URL, "https://"):
+				break
+			case strings.HasPrefix(location.URL, "http://"):
+				break
+			case strings.HasPrefix(location.URL, "https:/") || strings.HasPrefix(location.URL, "http:/"):
+				newStr := strings.Split(location.URL, ":/")
+				location.URL = newStr[0] + "://" + newStr[1]
+			case strings.HasPrefix(location.URL, "https:") || strings.HasPrefix(location.URL, "http:"):
+				newStr := strings.Split(location.URL, ":")
+				location.URL = newStr[0] + "://" + newStr[1]
+			case strings.HasPrefix(location.URL, "https//") || strings.HasPrefix(location.URL, "http//"):
+				newStr := strings.Split(location.URL, "//")
+				location.URL = newStr[0] + "://" + newStr[1]
+			case strings.HasPrefix(location.URL, "https/") || strings.HasPrefix(location.URL, "http/"):
+				newStr := strings.Split(location.URL, "//")
+				location.URL = newStr[0] + "://" + newStr[1]
+			default:
+				break
+			}
+
+		}
+	}
+
+	TLD := []string{
+		`\.com$`,
+		`\.org$`,
+		`\.gov$`,
+		`\.net$`,
+		`\.edu$`,
+		`\.mil$`,
+	}
+
+	var match bool = false
+	for i := 0; i < len(TLD); i++ {
+		tldMatch := regexp.MustCompile(TLD[i])
+		if tldMatch.MatchString(location.URL) {
+			match = true
+			break
+		}
+	}
+	if !match {
+		location.URL = location.URL + ".com" // figured defaulting to .com made the most sense.
+	}
+
 	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, location.URL, nil)
 	if err != nil {
-		return nil, err
+		return "", fmt.Errorf("error creating request: %w", err)
 	}
+
 	c := &http.Client{}
 	res, err := c.Do(req)
-	fmt.Println(res)
 	if err != nil {
-		return nil, err
+		return "", fmt.Errorf("request failed: %w", err)
 	}
-	data, err := io.Copy(buf, res.Body)
-	fmt.Println(data)
+	defer res.Body.Close()
+
+	_, err = io.Copy(buf, res.Body)
 	if err != nil {
-		return nil, err
+		return "", fmt.Errorf("error when reading the response body: %w", err)
 	}
-	var payload struct {
-		Data string `bson:"data"`
-	}
+
 	if err := json.Unmarshal(buf.Bytes(), &payload); err != nil {
-		return nil, err
+		return "", fmt.Errorf("error unmarshalling(totally a word) the response: %w", err)
 	}
+
 	return payload.Data, nil
 }
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Hiya!
+
+Ok, so I was a little confused on what exactly the test program was supposed to do, it seemed that was supposed to validate an output from an endpoint, but I couldn't validate that with the given endpoints. So I hope that was correct. :)
+
+If it was not, I'd love it if you let me know the scope of the assignment and I'll build it correctly.
+
+Hope y'all don't mind me, I just didn't want to fail on the premise of assignment confusion.
+
+Hope to speak to you soon!
+
+Brennan Woodbury
+435.830.0726
+brennanwoodbury@gmail.com
